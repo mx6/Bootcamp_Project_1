0f4185ab018bcc978e0591f07413e458d760cf6c
Matthew Adamson <matthewladamson@gmail.com>
2020-08-25 19:25:49 -0700

    Add files via upload
    Adding 'back to top of screen' button

--- script.js
+++ script.js
@@ -270,3 +270,28 @@ function maparea(lat, lon, trails) {
 function sortHikes(trails, user, weather) {
   console.log(trails, user, weather);
 }
+
+
+
+
+// scroll button 
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
