3c076f5c29e4050dd3743d6666bfe96efe6b128f
miadehaan <64979457+miadehaan@users.noreply.github.com>
2020-08-27 19:09:00 -0700

    5-day forecast added to screen w/ some styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -290,7 +290,7 @@ function forecast(lat, lon) {
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        console.log(response);
+        // console.log(response);
         
         $(".forecast").empty();
         // Get data for 5-day forecast
